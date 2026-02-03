4418c3630f78faffebf9394e0dcd36d60b55e3b0
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-02-03 14:56:39 +0100

    add: code updated with express

--- go-fullstack/backend/app.js
+++ go-fullstack/backend/app.js
@@ -4,5 +4,10 @@ const express = require('express');
 // Lance la méthode express pour créer une application
 const app = express();
 
+//Utilise la méthode use qui répondra à tout type de requête
+app.use((req, res) => {
+  res.json({message: 'Votre requête a bien été reçue !'});
+})
+
 // Exporte l'application
 module.exports = app;
--- go-fullstack/backend/server.js
+++ go-fullstack/backend/server.js
@@ -1,20 +1,25 @@
 // Importe le package http natif de Node pour pouvoir gérer les requetes http
 const http = require('http');
+// Importe l'application du fichier app
+const app = require('./app');
 
+// Défini un port d'écoute
+const port = process.env.PORT || 3000
+
+// Défini un port d'écoute pour l'application et l'engeriste avec la clé 'port'
+app.set('port', port)
 /*
 Crée un serveur avec la méthode du module http
-Il prend comme argument une fonction qui sera executée à chaque requête 
-recu par le serveur
-Cette fonction prend deux arguments : la requête et la réponse
+Il prend comme argument notre fonction app, qui va gére rles requêtes et les
+réponses
 */
-const server = http.createServer((req, res) => {
-  // réponse envoyé avec la méthode end
-  res.end('Voila la réponse du serveur !');
-});
+const server = http.createServer(app);
 
 /* 
-Spécifie le port d'écoute du serveur
+Spécifie le port d'écoute du serveur avec un message dans la console
 process.env.PORT prend le port d'écoute envoyé par l'environnement, sinon
 prend le port 3000 par défaut
 */
-server.listen(process.env.PORT || 3000);
+server.listen(port, () => {
+  console.log(`Server launched on port ${port}`);
+});
