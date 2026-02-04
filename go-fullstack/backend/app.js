8e52539084545dddf3cd4025970261a381295d6d
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-02-04 15:01:14 +0100

    fix: fixed the previous code for a clean version

--- go-fullstack/backend/app.js
+++ go-fullstack/backend/app.js
@@ -1,13 +1,26 @@
-// Importe le package express
 const express = require('express');
+const mongoose = require('mongoose');
 
-// Lance la méthode express pour créer une application
 const app = express();
 
-//Utilise la méthode use qui répondra à tout type de requête
-app.use((req, res) => {
-  res.json({message: 'Votre requête a bien été reçue !'});
-})
+mongoose.connect('mongodb+srv://Guillaume:MongoDBcluster137@mycluster.a8kns57.mongodb.net/?appName=MyCluster')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => console.log( error ));
 
-// Exporte l'application
+app.use(express.json());
+
+/* 
+Spécification des headers pour permettre de communiquer entre différents serveurs
+et éviter les erreurs CORS. Ils permettent :
+- d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
+- d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
+- d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
+*/
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 module.exports = app;
--- go-fullstack/backend/server.js
+++ go-fullstack/backend/server.js
@@ -1,25 +1,17 @@
-// Importe le package http natif de Node pour pouvoir gérer les requetes http
+// Importe le package http natif de node pour gérer les requêtes http
 const http = require('http');
-// Importe l'application du fichier app
+// Importe notre app 
 const app = require('./app');
 
 // Défini un port d'écoute
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-// Défini un port d'écoute pour l'application et l'engeriste avec la clé 'port'
-app.set('port', port)
-/*
-Crée un serveur avec la méthode du module http
-Il prend comme argument notre fonction app, qui va gére rles requêtes et les
-réponses
-*/
+// Spécifie de port d'écoute pour l'application
+app.set('port', port);
+
+// Lance le serveur
 const server = http.createServer(app);
 
-/* 
-Spécifie le port d'écoute du serveur avec un message dans la console
-process.env.PORT prend le port d'écoute envoyé par l'environnement, sinon
-prend le port 3000 par défaut
-*/
 server.listen(port, () => {
-  console.log(`Server launched on port ${port}`);
+  console.log(`Serveur lancé sur le port ${port}`);
 });
